65a10d4c441bae78e3647a417cdcd0cf0cd2d248
Tim Schwenke <tim.and.trallnag+code@gmail.com>
2020-12-02 23:18:57 +0100

    chore: Add import to ``__init__.py`

--- prometheus_alert_model/__init__.py
+++ prometheus_alert_model/__init__.py
@@ -1,2 +1,4 @@
 # Copyright © 2020 Tim Schwenke <tim.and.trallnag+code@gmail.com>
 # Licensed under Apache License 2.0 <http://www.apache.org/licenses/LICENSE-2.0>
+
+from .main import Alert, AlertGroup
