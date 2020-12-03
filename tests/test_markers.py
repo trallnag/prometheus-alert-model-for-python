bcbfe085409b34bb2c13e479805a19568e0cd3d0
Tim Schwenke <tim.and.trallnag+code@gmail.com>
2020-12-03 22:27:19 +0100

    ci: Add test for slow marker
    Else the test run returns code 5 because atm no slow tests exist.

---
+++ tests/test_markers.py
@@ -0,0 +1,13 @@
+# Copyright © 2020 Tim Schwenke <tim.and.trallnag+code@gmail.com>
+# Licensed under Apache License 2.0 <http://www.apache.org/licenses/LICENSE-2.0>
+
+import pytest
+
+
+@pytest.mark.slow
+def test_slow():
+    pass
+
+
+def test_not_slow():
+    pass
